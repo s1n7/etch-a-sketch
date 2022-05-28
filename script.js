32f825d0f9639639c08416b5e0036deaacff9abd
s1n7 <luisbruemmer@gmail.com>
2022-05-28 19:50:57 +0200

    Add 16 divs with JS and flex

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-const squareGrid = document.querySelector('#squareGrid');
+const container = document.querySelector('#container');
+
+
+for(let i = 0; i < 16; i++){
+    const div = document.createElement('div');
+    div.classList.add('squareDiv');
+    container.appendChild(div);
+}
