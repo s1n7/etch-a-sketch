1f1708a94f6f24bc599f84ee2250ecbd450c076a
s1n7 <luisbruemmer@gmail.com>
2022-05-30 19:27:21 +0200

    Create 16 x 16 div grid with js

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const container = document.querySelector('#container');
 
-
-for(let i = 0; i < 16; i++){
-    const div = document.createElement('div');
-    div.classList.add('squareDiv');
-    container.appendChild(div);
+//create 16 x 16 div grid
+for(let j = 0; j < 16; j++){
+    const colDiv = document.createElement('div');
+    colDiv.classList.add('col');
+    container.appendChild(colDiv);
+    for(let i = 0; i < 16; i++){
+        const squareDiv = document.createElement('div');
+        squareDiv.classList.add('squareDiv');
+        colDiv.appendChild(squareDiv);
+    }
 }
