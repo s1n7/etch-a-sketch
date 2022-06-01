5ecfcef546da97628023e5705112bafcef456d2f
s1n7 <luisbruemmer@gmail.com>
2022-06-01 18:10:57 +0200

    Make grid size variable and adjust squarediv size relatively to grid size

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 const container = document.querySelector('#container');
 
-//create 16 x 16 div grid
-for(let j = 0; j < 16; j++){
+//create custom size grid
+const gridSize = 4;
+const squareDivSize = 720/gridSize;
+for(let j = 0; j < gridSize; j++){
     const colDiv = document.createElement('div');
     colDiv.classList.add('col');
     container.appendChild(colDiv);
-    for(let i = 0; i < 16; i++){
+    for(let i = 0; i < gridSize; i++){
         const squareDiv = document.createElement('div');
+        squareDiv.style.width = `${squareDivSize}px`;
+        squareDiv.style.height = `${squareDivSize}px`;
         squareDiv.classList.add('squareDiv');
         colDiv.appendChild(squareDiv);
     }
